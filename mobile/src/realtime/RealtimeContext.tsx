78a9b333bae9a78baa7eea1abba0282f7ef7d052
didiradirara <didiradirara@gmail.com>
2025-09-05 09:18:54 +0900

    mid commit

--- mobile/src/realtime/RealtimeContext.tsx
+++ mobile/src/realtime/RealtimeContext.tsx
@@ -26,8 +26,8 @@ export function RealtimeProvider({ children }: { children: React.ReactNode }) {
     const extra: any = Constants.expoConfig?.extra || {};
 
     //const baseUrl: string = extra.API_BASE_URL || 'http://34.47.82.64';
-    const baseUrl: string = extra.API_BASE_URL || 'http://34.64.238.255';
-
+    //const baseUrl: string = extra.API_BASE_URL || 'http://34.64.238.255';
+    const baseUrl: string = extra.API_BASE_URL || 'http://localhost:4000';
     const socket: Socket = io(baseUrl, { transports: ['websocket'], auth: token ? { token } : undefined });
 
     socket.on('connect', () => setConnected(true));
